ae0161b8b7996bdaada26cfef130d1bc3bf10a6b
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-12-01 10:42:30 +0800

    Add logic for creating new project

---
+++ src/modules/Logic.js
@@ -0,0 +1,25 @@
+import { Project } from "./Project";
+
+let projects = [];
+
+const createNewProject = (title, description) => {
+  projects.push(new Project(title, description));
+  console.log(projects);
+  console.log(JSON.stringify(projects));
+}
+
+const saveToLocalStorage = () => {
+  localStorage.setItem('todo', JSON.stringify(projects));
+}
+
+const fetchItemsFromLocalStorage = () => {
+  if (localStorage.getItem('todo') == null) return;
+
+  return projects = JSON.parse(localStorage.getItem('todo'));
+}
+
+export {
+  createNewProject,
+  saveToLocalStorage,
+  fetchItemsFromLocalStorage
+}
