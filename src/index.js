6e19abedf8d24f7c9635932a8437a9f5e7a0bb3f
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-12-03 11:58:25 +0800

    Import bootstrap to js file instead of the template

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css';
 import './modules/EventListeners.js';
 import { startUpDOM } from './modules/DOM.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 window.onload = () => {
   startUpDOM.initiateProjects();
