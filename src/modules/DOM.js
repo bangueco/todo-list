4c8da29f35a93c1c1394316cd90768fe32e3e157
ivandnd <121280104+ivandnd@users.noreply.github.com>
2024-01-24 12:44:38 +0800

    Add modal dynamically

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -12,19 +12,34 @@ const newProjectDOM = (() => {
       const div = document.createElement('div');
       div.classList.add('project');
       const projectTitle = document.createElement('div');
+      const buttons = document.createElement('div');
+      const editButton = document.createElement('button');
       const deleteButton = document.createElement('button');
       projectTitle.classList.add('title');
+      editButton.classList.add('editButton');
+      editButton.classList.add('btn');
+      editButton.classList.add('btn-info');
+      editButton.classList.add('btn-sm');
+      editButton.dataset.bsToggle = 'modal';
+      editButton.dataset.bsTarget = '#editModal'
       deleteButton.classList.add('deleteButton');
       deleteButton.classList.add('btn');
-      deleteButton.classList.add('btn-danger')
+      deleteButton.classList.add('btn-danger');
+      deleteButton.classList.add('btn-sm');
+      buttons.classList.add('buttons');
       projectTitle.textContent = proj.title;
+      editButton.textContent = "EDIT";
       deleteButton.textContent = "DEL";
       div.dataset.projectIndex = index;
 
+      buttons.appendChild(editButton);
+      buttons.appendChild(deleteButton);
+
       div.appendChild(projectTitle);
-      div.appendChild(deleteButton);
+      div.appendChild(buttons);
       _projectsContainer.appendChild(div);
     });
+    appendEditButtonModal();
   }
 
   const insertNewProject = () => {
@@ -34,10 +49,101 @@ const newProjectDOM = (() => {
     showProjectsToContainer();
   }
 
+  const appendEditButtonModal = () => {
+    // Creating elements
+    const modal = document.createElement('div');
+    const modalDialog = document.createElement('div');
+    const modalContent = document.createElement('div');
+    const modalHeader = document.createElement('div');
+    const modalBody = document.createElement('div');
+    const modalFooter = document.createElement('div');
+
+    // Assigning classes and id
+    modal.classList.add('modal');
+    modal.classList.add('fade');
+    modal.id = 'editModal';
+    modalDialog.classList.add('modal-dialog');
+    modalContent.classList.add('modal-content');
+    modalHeader.classList.add('modal-header');
+    modalBody.classList.add('modal-body');
+    modalFooter.classList.add('modal-footer');
+    
+    // Append modal header
+    const h1 = document.createElement('h1');
+    h1.classList.add('modal-title');
+    h1.classList.add('fs-5');
+    h1.id = 'editModalLabel';
+    h1.textContent = "Edit project information";
+    modalHeader.appendChild(h1);
+
+    // Append modal button
+    const x = document.createElement('button');
+    x.setAttribute('type', 'button');
+    x.classList.add('btn-close');
+    x.dataset.bsDismiss = 'modal';
+    modalHeader.appendChild(x);
+
+    // Append modal body
+    const projectNameContainer = document.createElement('div');
+    const projectDescriptionContainer = document.createElement('div');
+
+    const projectNameLabel = document.createElement('label');
+    const projectDescriptionLabel = document.createElement('label');
+    projectNameLabel.textContent = 'Project Name';
+    projectNameLabel.classList.add('form-label');
+    projectDescriptionLabel.textContent = 'Project Description'
+    projectDescriptionLabel.classList.add('form-label');
+    projectNameContainer.appendChild(projectNameLabel);
+    projectDescriptionContainer.appendChild(projectDescriptionLabel);
+    const projectNameInput = document.createElement('input');
+    projectNameInput.classList.add('form-control')
+    projectNameInput.id = 'editProjectName';
+    projectNameInput.type = "text";
+    const projectDescriptionInput = document.createElement('input');
+    projectDescriptionInput.classList.add('form-control')
+    projectDescriptionInput.id = 'editProjectDescription';
+    projectDescriptionInput.type = "text";
+
+    projectNameContainer.appendChild(projectNameInput);
+    projectDescriptionContainer.appendChild(projectDescriptionInput);
+    modalBody.appendChild(projectNameContainer);
+    modalBody.appendChild(projectDescriptionContainer);
+
+    // Append modal footer
+    const close = document.createElement('button')
+    close.setAttribute('type', 'button');
+    close.classList.add('btn');
+    close.classList.add('btn-secondary');
+    close.dataset.bsDismiss = "modal";
+    close.textContent = "Close";
+
+    const editSubmitBtn = document.createElement('button');
+    editSubmitBtn.setAttribute('type', 'button');
+    editSubmitBtn.id = "editButtonProject";
+    editSubmitBtn.classList.add('btn');
+    editSubmitBtn.classList.add('btn-success');
+    editSubmitBtn.dataset.bsDismiss = "modal";
+    editSubmitBtn.textContent = "Submit";
+
+    modalFooter.appendChild(close);
+    modalFooter.appendChild(editSubmitBtn);
+
+    // Append elements
+    modalDialog.appendChild(modalContent);
+    modalContent.appendChild(modalHeader);
+    modalContent.appendChild(modalBody);
+    modalContent.appendChild(modalFooter);
+    modal.appendChild(modalDialog);
+
+
+    _projectsContainer.appendChild(modal);
+  }
+
   return {
     addProjectBtn,
     showProjectsToContainer,
     insertNewProject,
+    appendEditButtonModal
   }
 })();
 
